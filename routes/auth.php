db672ef324845ac3ced314cd7a889c314c672003
Fuad Hamdi Bahar <fuad.bahar@iconpln.co.id>
2025-05-27 16:20:14 +0700

    finish #10

--- routes/auth.php
+++ routes/auth.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\AuthenticateController;
-
+use App\Http\Controllers\Auth\EmailVerificationController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
@@ -17,4 +17,9 @@ Route::middleware('guest')->group(function () {
 
 Route::middleware('auth')->group(function () {
     Route::post('/logout', [AuthenticateController::class, 'destroy'])->name('logout');
+
+    // Email Verification
+    Route::get('/email/verify', [EmailVerificationController::class, 'notice'])->name('verification.notice');
+    Route::get('/email/verify/{id}/{hash}', [EmailVerificationController::class, 'handler'])->middleware('signed')->name('verification.verify');
+    Route::post('/email/verification-notification', [EmailVerificationController::class, 'resend'])->middleware('throttle:6,1')->name('verification.send');
 });
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
+// if verify email works
+// Route::inertia('/', 'Home')->middleware('verified')->name('home');
 Route::inertia('/', 'Home')->name('home');
 
 require __DIR__ . '/auth.php';
